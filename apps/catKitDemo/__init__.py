24c001d5facef65bd9805accd4ec0262d792ae74
Max Hoffmann <mjhoffmann@gmail.com>
2018-02-05 14:43:13 -0800

    Added custom placeholder and siteType options for adsorption site adderE

--- apps/catKitDemo/__init__.py
+++ apps/catKitDemo/__init__.py
@@ -28,7 +28,8 @@ import catkit.surface
 
 catKitDemo = flask.Blueprint('catKitDemo', __name__)
 
-VALID_OUT_FORMATS = ["abinit", "castep-cell", "cfg", "cif", "dlp4", "eon", "espresso-in", "extxyz", "findsym", "gen", "gromos", "json", "jsv", "nwchem", "proteindatabank", "py", "turbomole", "v-sim", "vasp", "xsf", "xyz"]
+VALID_OUT_FORMATS = ["abinit", "castep-cell", "cfg", "cif", "dlp4", "eon", "espresso-in", "extxyz", "findsym",
+                     "gen", "gromos", "json", "jsv", "nwchem", "proteindatabank", "py", "turbomole", "v-sim", "vasp", "xsf", "xyz"]
 
 
 @catKitDemo.route('/generate_bulk_cif/', methods=['GET', 'POST'])
@@ -43,20 +44,9 @@ def generate_bulk_cif(request=None):
         'bulkParams', '{}')).get('structure', 'fcc')
     lattice_constant = float(json.loads(request.args.get(
         'bulkParams', '{}')).get('lattice_constant', 4.0))
-    element1 = json.loads(request.args.get(
-        'bulkParams', '{}')).get('element1', 'Pt')
-    element2 = json.loads(request.args.get(
-        'bulkParams', '{}')).get('element2', 'Pt')
-    element3 = json.loads(request.args.get(
-        'bulkParams', '{}')).get('element3', 'Pt')
-    element4 = json.loads(request.args.get(
-        'bulkParams', '{}')).get('element4', 'Pt')
-    elements = [
-        element1,
-        element2,
-        element3,
-        element4,
-    ]
+
+    elements = json.loads(request.args.get('bulkParams', '{}')).get('elements')
+    pprint.pprint(json.loads(request.args.get('bulkParams', '{}')))
 
     for i in range(1, 5):
         try:
@@ -134,6 +124,8 @@ def get_adsorption_sites(request=None):
     if type(request.args) is str:
         request.args = json.loads(request.args)
 
+    pprint.pprint(request.args)
+
     miller_x = int(json.loads(request.args.get(
         'slabParams', '{}')).get('miller_x', 1))
     miller_y = int(json.loads(request.args.get(
@@ -148,6 +140,12 @@ def get_adsorption_sites(request=None):
     bulk_cif = str(request.args.get(
         'bulk_cif', (json.loads(generate_bulk_cif(request).data)['cifdata'])))
     cif_images = json.loads(generate_slab_cif(request).data)['images']
+
+    place_holder = str(json.loads(request.args.get(
+        'adsorbateParams', '{}')).get('placeHolder', 'F'))
+
+    site_type = str(json.loads(request.args.get(
+        'adsorbateParams', '{}')).get('siteType', 'all'))
 
     # create bulk atoms
     mem_file = StringIO.StringIO()
@@ -189,32 +187,33 @@ def get_adsorption_sites(request=None):
         )
         atoms = gen.get_slab(primitive=True)
         sites = gen.adsorption_sites(
-                atoms,
-                symmetry_reduced=True,
-                )
+            atoms,
+            symmetry_reduced=True,
+        )
         sites_list.append(sites)
         label_index = 0
         alt_labels.append({})
         for site_label in sorted(sites):
+            if site_type != 'all' and site_label != site_type:
+                continue
             for site_label_i, site in enumerate(sites[site_label][0]):
                 if len(site) > 0:
-                    atoms += ase.atom.Atom('F', site + [0., 0., 1.5])
+                    atoms += ase.atom.Atom(place_holder, site + [0., 0., 1.5])
                     natoms = len(atoms) - 1
-                    alt_labels[-1][len(atoms)-1] = site_label + ' ' + str(site_label_i)
+                    alt_labels[-1][len(atoms) - 1] = site_label + \
+                        ' ' + str(site_label_i)
                     label_index += 1
 
         with StringIO.StringIO() as f:
             ase.io.write(f, atoms, format='cif')
             cif_images.append(f.getvalue())
 
-
-
-    ## serialize numpy arrays
-    #for i, sites in enumerate(sites_list):
-        #for j, site_name in enumerate(sites):
-            #for k, site in enumerate(sites_list[i][site_name]):
-                #if type(site) is np.ndarray:
-                    #sites_list[i][site_name][k] = site.tolist()
+    # serialize numpy arrays
+    # for i, sites in enumerate(sites_list):
+        # for j, site_name in enumerate(sites):
+            # for k, site in enumerate(sites_list[i][site_name]):
+            # if type(site) is np.ndarray:
+            #sites_list[i][site_name][k] = site.tolist()
 
     return flask.jsonify({
         'data': (sites_list),
@@ -283,8 +282,8 @@ def place_adsorbates(request=None):
         )
         atoms = gen.get_slab(primitive=True)
         sites = gen.adsorption_sites(
-                atoms, symmetry_reduced=True,
-                )
+            atoms, symmetry_reduced=True,
+        )
         for k in sorted(sites):
             v = sites[k]
             if len(v) != 3:
@@ -295,9 +294,11 @@ def place_adsorbates(request=None):
             for j, site in enumerate(positions):
                 occupation = site_occupation.get(str(i), {}).get(str(k), {})[j]
                 if occupation != 'empty':
-                    atoms += ase.atoms.Atoms(occupation, [site + np.array([0, 0, 1.5])])
+                    atoms += ase.atoms.Atoms(occupation,
+                                             [site + np.array([0, 0, 1.5])])
                 else:
-                    atoms += ase.atoms.Atoms('F', [site + np.array([0, 0, 1.5])])
+                    atoms += ase.atoms.Atoms('F',
+                                             [site + np.array([0, 0, 1.5])])
         images[i] = atoms
 
     mem_files = []
@@ -331,27 +332,29 @@ def generate_dft_input(request=None):
 
     # Generate Zip File
     ####################
-    timestr = time.strftime("%Y%m%d_%H%M%S", datetime.datetime.now().timetuple())
+    timestr = time.strftime(
+        "%Y%m%d_%H%M%S", datetime.datetime.now().timetuple())
     calcstr = "calculations_{timestr}".format(**locals())
     mem_file = StringIO.BytesIO()
     zf = zipfile.ZipFile(mem_file, 'w')
     zf.writestr(
-            '{calcstr}/publication.txt'.format(**locals()),
-            '{"volume": "",\n"publisher": "",\n"doi": "",\n"title": "",\n"journal": "",\n"authors": [],\n"year": "",\n"number": "",\n"pages": ""}\n')
+        '{calcstr}/publication.txt'.format(**locals()),
+        '{"volume": "",\n"publisher": "",\n"doi": "",\n"title": "",\n"journal": "",\n"authors": [],\n"year": "",\n"number": "",\n"pages": ""}\n')
 
     # Here be Dragons
     ####################
 
-
     zf.close()
     mem_file.seek(0)
     response = flask.send_file(
-            mem_file,
-            attachment_filename="{calcstr}.zip".format(**locals()),
-            )
+        mem_file,
+        attachment_filename="{calcstr}.zip".format(**locals()),
+    )
 
-    response.headers[u"Content-Disposition"] = 'attachment; filename="{calcstr}.zip"'.format(**locals())
+    response.headers[
+        u"Content-Disposition"] = 'attachment; filename="{calcstr}.zip"'.format(**locals())
     return response
+
 
 @catKitDemo.route('/convert_atoms/', methods=['GET', 'POST'])
 def convert_atoms(request=None):
@@ -367,7 +370,7 @@ def convert_atoms(request=None):
     if out_format not in VALID_OUT_FORMATS:
         return {
             "error": "outFormat {outformat} is invalid. Should be on of {VALID_OUT_FORMATS}".format(**locals()),
-                }
+        }
 
     with StringIO.BytesIO() as in_bfile:
         request.files['file'].save(in_bfile)
@@ -378,23 +381,29 @@ def convert_atoms(request=None):
                 in_file.write(in_bfile.getvalue().decode('UTF-8'))
             except Exception as error:
                 in_file = in_bfile
-                #return flask.jsonify({
-                    #'error': 'Binary files not supported, yet.\n{error}'.format(**locals())
-                    #})
+                # return flask.jsonify({
+                #'error': 'Binary files not supported, yet.\n{error}'.format(**locals())
+                #})
             in_file.seek(0)
             filetype = ase.io.formats.filetype(filename, read=False)
+            # Monkey-patching ase.io.formats
+            if filetype == 'inp':
+                filetype = 'espresso-in'
+            print("FILETYPE '{filetype}'".format(**locals()))
             try:
                 atoms = ase.io.read(
-                        filename=in_file,
-                        index=None,
-                        format=filetype,
-                        #io=ase.io.formats.get_ioformat(filetype),
-                        #parallel=False,
-                        )
+                    filename=in_file,
+                    index=None,
+                    format=filetype,
+                )
             except Exception as error:
-                return flask.jsonify({
-                    'error': 'Binary files not supported, yet.\nfiletype = {filetype}\n{error}'.format(**locals())
+                if filetype == 'espresso-in':
+                    return flask.jsonify({
+                        'error': 'Upload Error: Need QE >=5.3 Input File.'.format(**locals())
                     })
+                return flask.jsonify({
+                    'error': 'Upload Error: filetype = {filetype}\n{error}'.format(**locals())
+                })
             with StringIO.StringIO() as out_file:
                 out_file.name = 'CatApp Browser Export'
                 ase.io.write(out_file, atoms, out_format)
@@ -405,6 +414,28 @@ def convert_atoms(request=None):
         'input_filetype': filetype,
         'output_filetype': out_format,
         'filename': filename,
-        })
+    })
 
 
+@catKitDemo.route('/upload_dataset/', methods=['GET', 'POST'])
+def upload_dataset(request=None):
+    request = flask.request if request is None else request
+    filename = request.files['file'].filename
+    print(filename)
+    suffix = os.path.splitext(filename)[1]
+    if suffix == '.zip':
+        message = ("You uploaded a zip file")
+        suffix = 'ZIP'
+    elif suffix in ['.tgz', '.tar.gz']:
+        message = ("You uploaded a tar file.")
+        suffix = 'TGZ'
+    else:
+        message = 'Could not detect filetype.'
+        suffix = None
+
+    with StringIO.BytesIO() as in_bfile:
+        request.files['file'].save(in_bfile)
+
+    return flask.jsonify({
+        'message': message,
+    })
