a68757be9b5e211d6a1ef40fd9bbdadfd169a6b4
Max Hoffmann <mjhoffmann@gmail.com>
2018-04-05 11:59:35 -0700

    Remove import of pourbaix before it is ready

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ class NumpyEncoder(json.JSONEncoder):
             return obj.tolist()
         else:
             return super(NumpyEncoder, self).default(obj)
-from apps.pourbaix.run_pourbaix import pourbaix
+#from apps.pourbaix.run_pourbaix import pourbaix
 
 app = flask.Flask(__name__)
 app.debug = True
@@ -64,7 +64,7 @@ def apps():
 
 # Blueprint
 #app.register_blueprint(atoml_blueprint)
-app.register_blueprint(pourbaix, url_prefix='/apps/pourbaix')
+#app.register_blueprint(pourbaix, url_prefix='/apps/pourbaix')
 
 # Graphql view
 app.add_url_rule('/graphql',
