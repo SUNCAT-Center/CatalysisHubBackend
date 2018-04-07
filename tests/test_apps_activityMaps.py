7586ce68c720c5c1b824021b88cb6d74bb185b08
Max Hoffmann <mjhoffmann@gmail.com>
2018-04-07 13:08:35 -0700

    Added a regression teset

---
+++ tests/test_apps_activityMaps.py
@@ -0,0 +1,40 @@
+import os
+import sys
+import unittest
+import tempfile
+import pprint
+import sqlite3
+import json
+
+import flask
+
+sys.path.append(os.path.abspath('.'))
+
+import app
+
+class ReactionBackendTestCase(unittest.TestCase):
+    def setUp(self):
+        #TEST_DB_FILENAME = './test_database.db'
+        #self.db_fd = open(TEST_DB_FILENAME, 'w',)
+        #app.app.config['DATABASE'] = TEST_DB_FILENAME
+        #self.db_fd, app.app.config['DATABASE'] = 'test_data'
+        #os.environ['SQLITE_DB'] = app.app.config['DATABASE']
+        app.app.testing = True
+        self.app = app.app.test_client()
+        #with app.app.app_context():
+        #    init_db(app.app)
+
+
+    def test_blank_reaction_query(self):
+        data = json.loads(
+                self.app.get('/apps/activityMaps/systems/?activityMap=blank')
+                .data
+                )
+
+        
+        test_data = {'reference': '',
+                        'systems': [],
+                        'xlabel': '',
+                        'ylabel': '',
+                        'zlabel': ''}
+        assert data == test_data, (data, test_data)
