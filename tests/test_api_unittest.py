a142653dc4cddb0f2be7a68552419c6dff268a41
Max Hoffmann <mjhoffmann@gmail.com>
2018-04-12 22:58:53 -0700

    Added correct test results

--- tests/test_api_unittest.py
+++ tests/test_api_unittest.py
@@ -231,19 +231,19 @@ class ReactionBackendTestCase(unittest.TestCase):
 
     def test_facet_property(self):
         rv_data = self.get_data('{systems(last: 10) { edges { node { uniqueId Facet } } }}')
-        assert False, rv_data
+        assert rv_data['data']['systems']['edges'][0]['node']['Facet'] == '1x1x1', rv_data
 
     def test_username_property(self):
         rv_data = self.get_data('{systems(last: 10) { edges { node { uniqueId Username } } }}')
-        assert False, rv_data
+        assert rv_data['data']['systems']['edges'][0]['node']['Username'] == '', rv_data
 
     def test_adsorbate_property(self):
         rv_data = self.get_data('{systems(last: 10) { edges { node { uniqueId Adsorbate } } }}')
-        assert False, rv_data
+        assert rv_data['data']['systems']['edges'][0]['node']['Adsorbate'] == '', rv_data
 
     def test_reaction_property(self):
         rv_data = self.get_data('{systems(last: 10) { edges { node { uniqueId Reaction } } }}')
-        assert False, rv_data
+        assert rv_data['data']['systems']['edges'][0]['node']['Reaction'] == '', rv_data
 
 if __name__ == '__main__':
     unittest.main()
