ab4fe9b0d20dc43cecd0ca9026ce980acaf6e43a
Max Hoffmann <mjhoffmann@gmail.com>
2018-07-11 17:35:24 -0700

    Explicit set headers in app

--- app.py
+++ app.py
@@ -117,6 +117,7 @@ def after_request(response):
     response.headers.add('Access-Control-Allow-Headers', 'X-PINGOTHER, Content-Type, Authorization, Accept')
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,HEAD,OPTIONS')
     #response.headers.add('Access-Control-Allow-Origin', 'http://localhost:3000')
+    response.headers.add('Access-Control-Allow-Origin', 'https://www.catalysis-hub.org')
     return response
 
 logging.getLogger('flask_cors').level = logging.DEBUG
