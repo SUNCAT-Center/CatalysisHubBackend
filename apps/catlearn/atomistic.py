441d911f5d5867833f1c9c49c81be05dc1bd240a
Kirsten Winther <winther@stanford.edu>
2018-10-23 14:04:10 -0700

    more imports

--- apps/catlearn/atomistic.py
+++ apps/catlearn/atomistic.py
@@ -8,7 +8,7 @@ from sklearn.preprocessing import Imputer
 import ase.atoms
 
 from catlearn.featurize.adsorbate_prep import autogen_info
-from catlearn.fingerprint.setup import FeatureGenerator, default_fingerprinters
+from catlearn.featurize.setup import FeatureGenerator, default_fingerprinters
 from catlearn.regression.gpfunctions import io as gp_io
 
 
